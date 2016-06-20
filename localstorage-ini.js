5fe5e8215b4b72026fdc5d7df643de0fe0d21812
martinderm <martin@convive.io>
2016-06-20 16:18:54 +0200

    New API URLs

--- localstorage-ini.js
+++ localstorage-ini.js
@@ -7,22 +7,22 @@ function localstorageIni() {
       if (lang=='de'){
         localStorage.setItem("lang",JSON.stringify('de_DE'));
         localStorage.setItem("lang_wp",321);
-        localStorage.setItem("translation", $.getJSON( "http://cn.convive.io/wp-json/wp/v2/posts/321" ));
+        localStorage.setItem("translation", $.getJSON( "http://climate-neutral.org/wp/wp-json/wp/v2/posts/321" ));
     }
       else if (lang=='fr'){
         localStorage.setItem("lang",JSON.stringify('fr_FR'));
         localStorage.setItem("lang_wp",720);
-        localStorage.setItem("translation", $.getJSON( "http://cn.convive.io/wp-json/wp/v2/posts/720" ));
+        localStorage.setItem("translation", $.getJSON( "http://climate-neutral.org/wp/wp-json/wp/v2/posts/720" ));
     }
       else if (lang=='es'){
         localStorage.setItem("lang",JSON.stringify('es_ES'));
         localStorage.setItem("lang_wp",722);
-        localStorage.setItem("translation", $.getJSON( "http://cn.convive.io/wp-json/wp/v2/posts/722" ));
+        localStorage.setItem("translation", $.getJSON( "http://climate-neutral.org/wp/wp-json/wp/v2/posts/722" ));
     }
       else {
         localStorage.setItem("lang",JSON.stringify('en_EN'));
         localStorage.setItem("lang_wp",323);
-        localStorage.setItem("translation", $.getJSON( "http://cn.convive.io/wp-json/wp/v2/posts/323" ));
+        localStorage.setItem("translation", $.getJSON( "http://climate-neutral.org/wp/wp-json/wp/v2/posts/323" ));
     }
   };
 }
